380d6709b56f09a9c0761703c86ff5bffd4ffef5
Abhi-webd <83855664+Abhi-webd@users.noreply.github.com>
2022-05-26 11:48:42 +0530

    Update 2.triplets.cpp

--- 2.triplets.cpp
+++ 2.triplets.cpp
@@ -43,3 +43,46 @@ int main()
 
     return 0;
 }
+
+
+//sum equal to zero and duplicate not allowed
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& a) {
+        int n =  a .size();
+        
+    sort(a.begin(),a.end());
+    vector<vector<int>> ans;
+        if(n < 3){
+            return ans;
+        }
+    for(int i = 0 ; i <= n-3 ; i++){
+        int first = i;
+        int second = i+1;
+        int third = n-1;
+
+        while(second < third){
+            int sum = a[first] + a[second] + a[third];
+            if(sum == 0){
+                ans.push_back({a[first],a[second],a[third]});
+                while(third != 0 && a[third] == a[third - 1]) third--;
+                while(second != n-1 && a[second] == a[second + 1]) second++;
+                second++;
+                third--;
+            }
+            else if(sum > 0){
+                while(third != 0 && a[third] == a[third - 1]) third--;
+                third -= 1;
+            }
+            else{
+                while(second != n-1 && a[second] == a[second + 1]) second++;
+                second += 1;
+            }
+        }
+        while(i != n-1 && a[i] == a[i+1]) i++;
+    }
+    
+        return ans;
+    }
+};
