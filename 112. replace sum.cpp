05478acd84555ac69e4e3b1f98e6f978117e3db9
Abhi-webd <83855664+Abhi-webd@users.noreply.github.com>
2022-06-22 09:53:38 +0530

    Create 112. replace sum.cpp

---
+++ 112. replace sum.cpp
@@ -0,0 +1,13 @@
+int replaceWithSum(node* root){
+    if(root == NULL){
+        return 0;
+    }
+    if(root->left == NULL && root->right == NULL){
+        return root->data;
+    }
+    int LS = replaceWithSum(root->left);
+    int RS = replaceWithSum(root->right);
+    int temp = root->data;
+    root->data = LS+RS;
+    return root->data+temp;
+}
