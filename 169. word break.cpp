579467ea37ab2069f53b3b8ee3f37ee753af0264
Abhi-webd <83855664+Abhi-webd@users.noreply.github.com>
2022-07-05 11:49:54 +0530

    Create 169. word break.cpp

---
+++ 169. word break.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    
+    bool helper(int i , string s, set<string> &st,vector<int> &dp){
+        if(i == s.size()){
+            return 1;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        string temp;
+        for(int j = i ; j < s.size() ; j++){
+            temp += s[j];
+            if(st.find(temp) != st.end()){
+                if(helper(j+1,s,st,dp)){
+                    return dp[i] = 1;
+                }
+            }
+        }
+        return dp[i] = 0;
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        set<string> st;
+        vector<int> dp(301,-1);
+        for(auto i : wordDict){
+            st.insert(i);
+        }
+        return helper(0,s,st,dp);
+    }
+};
