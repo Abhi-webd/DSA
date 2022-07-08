d3892db94124c53f154316499071a0ea7a2da702
Abhi-webd <83855664+Abhi-webd@users.noreply.github.com>
2022-07-08 08:00:05 +0530

    Create 189. tree from inorder and preorder .cpp

---
+++ 189. tree from inorder and preorder .cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, int> inorderMap;
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        for (int i = 0; i < inorder.size(); i++) inorderMap[inorder[i]] = i;
+        return addNode(preorder, 0, 0, preorder.size());
+    }
+
+    TreeNode* addNode(vector<int>& preorder, int preBegin, int begin, int size) {
+        int val = preorder[preBegin];
+        TreeNode* cur = new TreeNode(val, nullptr, nullptr);
+        int inorderIndex = inorderMap[val];
+        int leftSize = inorderIndex - begin;
+        int rightSize = size - leftSize - 1;
+        if (leftSize) cur->left = addNode(preorder, preBegin + 1, begin, leftSize);
+        if (rightSize) cur->right = addNode(preorder, preBegin + leftSize + 1, begin + leftSize + 1, rightSize);
+
+        return cur;
+    }
+};
