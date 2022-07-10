301ec39c4aa2adb0c905ab655209e661f0da34de
Abhi-webd <83855664+Abhi-webd@users.noreply.github.com>
2022-07-10 20:12:46 +0530

    Create 207.kth largest element in the stream.cpp

---
+++ 207.kth largest element in the stream.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    vector<int> kthLargest(int k, int arr[], int n) {
+        // code here
+        vector<int>ans;
+        priority_queue<int, vector<int>, greater<int>>pq;
+        for(int i=0; i<n; i++)
+        {
+            pq.push(arr[i]);
+            if(pq.size() < k)
+            ans.push_back(-1);
+            else if(pq.size() == k)
+            ans.push_back(pq.top());
+            else
+            {
+                pq.pop();
+                ans.push_back(pq.top());
+            }
+        }
+        return ans;
+    }
+};
