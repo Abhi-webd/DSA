fb879d2c8753e227140c0be5be3283aa22421030
Abhi-webd <83855664+Abhi-webd@users.noreply.github.com>
2022-07-15 13:26:38 +0530

    Update 256. find winner.cpp

--- 256. find winner.cpp
+++ 256. find winner.cpp
@@ -1,1 +1,13 @@
-
+int findWinner(int n, int x, int y)
+	{
+		// Your code goes here
+		 vector<int> dp(n+1,0);
+         dp[0]=0;
+         for(int i=0;i<=n;++i){
+             if(dp[i]==0){
+         if(i+1<=n) dp[i+1]=1;
+         if(i+x <= n) dp[i+x]=1;
+         if(i+y <= n) dp[i+y]=1;}
+     }
+		return dp[n];
+	}
